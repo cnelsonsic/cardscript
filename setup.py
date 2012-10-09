3dd68bf168a1408b92e0170ed5193826808d40a3
C Nelson <cnelsonsic@gmail.com>
2012-10-08 20:32:39 -0500

    More additions to setup.py plus a LICENSE.

--- setup.py
+++ setup.py
@@ -3,8 +3,23 @@ from distutils.core import setup
 setup(
     name='cardscript',
     version='0.6',
+    description="A scriptable card game processing engine.",
+    author="Charles Nelson",
+    author_email="cnelsonsic@gmail.com",
+    url="https://github.com/cnelsonsic/cardscript",
     packages=['cardscript', 'cardscript.cards'],
-    license='AGPL',
+    license='AGPLv3+',
     long_description=open('README.md').read(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+        'Intended Audience :: End Users/Desktop',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Games/Entertainment :: Board Games',
+        ],
 )
 
